e396352aa97efbd5d7c93bca1725550d541fcf13
Daisuke Osanai <dsk.osanai@gmail.com>
2022-04-17 20:24:23 +0900

    no comment

---
+++ lib/riverpod+changeNotifier/color_viewModel.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final ColorProvider = ChangeNotifierProvider((ref) => Color());
+
+class Color extends ChangeNotifier {
+  var _colorNumber = 5;
+  int get colorNumber => _colorNumber;
+
+  void changeColor() {
+    if (colorNumber == 18) {
+      _colorNumber = 0;
+    } else {
+      _colorNumber++;
+    }
+    notifyListeners();
+  }
+}
